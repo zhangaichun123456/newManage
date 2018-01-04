9f16258045c8e2f029742c8935dd71d7cfec057c
zhangaichun123456 <113120256@qq.com>
2018-01-04 17:16:53 +0800

    构建整体框架

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
-Vue.config.productionTip = false
+import '@/common/scss/index.scss'
+
+Vue.config.productionTip = false;
+Vue.prototype.$ajax = axios;
+axios.defaults.withCredentials=true;//用axios发送post请求去登录，能成功返回数据，但是用作权限验证的cookie就是没有保存,经查阅，axios 默认不发送cookie，跨域也是一个原因，需要全局设置
+
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from './home'
+
+
+const MyLogin = () => import('@/components/MyLogin/MyLogin')
 
 Vue.use(Router)
 
@@ -8,8 +11,10 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'login',
+      component: MyLogin
+    },
+    // 首页
+    ...home,
   ]
 })
